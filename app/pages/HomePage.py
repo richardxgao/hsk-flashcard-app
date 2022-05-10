56f1cb9d76215fcec69badb2a05d28ec791218fe
Richard <richardqxgao@gmail.com>
2022-05-10 11:04:24 -0600

    Add styling for buttons

--- app/pages/HomePage.py
+++ app/pages/HomePage.py
@@ -1,4 +1,4 @@
-from tkinter.ttk import Button
+from tkinter.ttk import Button, Style
 from tkinter import Label
 from tkinter import Frame
 
@@ -9,17 +9,19 @@ class HomePage(Frame):
 
         Label(self, text="HSK Flashcard App", font=("Arial", 72)).pack(expand=True)
 
+        self.style = Style()
+        self.style.configure("TButton", font=("Arial", 20), background="blue")
+
         buttons_frame = Frame(self)
         buttons_frame.pack()
 
-        Label(buttons_frame, text="Choose a deck to begin:").grid(row=0, column=0, columnspan=3)
-        Button(buttons_frame, text="HSK1", command=lambda: parent.show_page(parent.pages['HSK1'])).grid(row=1, column=0, padx=5, pady=5)
+        Label(buttons_frame, text="Choose a deck to begin:").grid(row=0, column=0, columnspan=2)
+        Button(buttons_frame, text="HSK1", command=lambda: parent.show_page(parent.pages['HSK1']),
+               style="TButton").grid(row=1, column=0, padx=5, pady=5)
         Button(buttons_frame, text="HSK2", command=lambda: parent.show_page(parent.pages['HSK2'])).grid(row=2, column=0, padx=5, pady=5)
         Button(buttons_frame, text="HSK3", command=lambda: parent.show_page(parent.pages['HSK3'])).grid(row=3, column=0, padx=5, pady=5)
         Button(buttons_frame, text="HSK4", command=lambda: parent.show_page(parent.pages['HSK4'])).grid(row=1, column=1, padx=5, pady=5)
         Button(buttons_frame, text="HSK5", command=lambda: parent.show_page(parent.pages['HSK5'])).grid(row=2, column=1, padx=5, pady=5)
         Button(buttons_frame, text="HSK6", command=lambda: parent.show_page(parent.pages['HSK6'])).grid(row=3   , column=1, padx=5, pady=5)
-        Button(buttons_frame, text="Load Deck").grid(row=1, column=2, padx=5, pady=5)
-        Button(buttons_frame, text="Custom Select").grid(row=2, column=2, padx=5, pady=5)
-        Button(buttons_frame, text="Starred Deck").grid(row=3, column=2, padx=5, pady=5)
+        Button(buttons_frame, text="Load Deck").grid(row=4, column=0, padx=5, pady=5, columnspan=2)
 
