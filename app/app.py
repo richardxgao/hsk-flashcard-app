08da126a0062ef81408a8ac86076a462ffeb7adc
Richard <richardqxgao@gmail.com>
2022-05-10 11:04:56 -0600

    Add keyboard shortcuts, save decks to file on close

--- app/app.py
+++ app/app.py
@@ -8,11 +8,15 @@ from pages.DeckPage import DeckPage
 class HSKFlashcardApp(Tk):
     def __init__(self):
         super().__init__()
+
         self.title("HSK Flashcard App")
+
         self.WIDTH, self.HEIGHT = 1100, 700
         self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
         self.place_in_center_of_screen()
-        # self.geometry("+500+80")  # Place window in center of screen.
+
+        self.bind("<Down>", lambda event: self.exit_app())
+
         self.option_add("*Font", ("Arial", 24))
         self.option_add("*background", "#1F2024")
         self.option_add("*foreground", "#83B5D1")
@@ -43,8 +47,14 @@ class HSKFlashcardApp(Tk):
 
     def place_in_center_of_screen(self):
         screen_width, screen_height = self.winfo_screenwidth(), self.winfo_screenheight()
-        place_coordinates = (int((screen_width - self.WIDTH)/2), int((screen_height - self.HEIGHT)/2))
+        place_coordinates = (int((screen_width - self.WIDTH)/2), int((screen_height - self.HEIGHT)/4))
         self.geometry(f"+{place_coordinates[0]}+{place_coordinates[1]}")
+
+    def exit_app(self):
+        for page in self.pages:
+            if page != "HomePage":
+                self.pages[page].save_deck()
+        exit()
 
 
 if __name__ == '__main__':
